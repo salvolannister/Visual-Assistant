0bb06ca7bde157a4f0f32fa47ab5152c9477d26c
salvolannister <salvatore.d.gurzi@gmail.com>
2019-08-04 22:26:48 -0300

    aggiunto algoritmo per individuare rettangoli

--- Tutorial_earth.py
+++ Tutorial_earth.py
@@ -10,46 +10,46 @@ def main():
     # indica la webcam
     cap = cv2.VideoCapture(0)
     t = 100 # treshold for canny detection edge
-    w = 640 #width we want
+    w = 640.0 #width we want
     #abilita modo interattivo, va copiato su
     plt.ion()
     fig = plt.figure()
     #a questa figura ci agganciamo
     fig.canvas.mpl_connect("close event", lambda event: handle_close(event)) #quando chiudiamo la finestra si chiuderà anche il video!
     #inizializzazione
-    img = None
+    image = None
     while cap.isOpened():
         # ret viene usato in caso di errore
-        ret, frame = cap.read()
-        width, height, depth = frame.shape
-        scale = w/ width
-        h = height
+        ret, image = cap.read()
+        width, height, depth = image.shape
+        scale = w / width
+        h = height * scale;
 
-        frame = cv2.resize(frame, (0, 0), fx=scale, fy=scale)
+        image = cv2.resize(image, (0, 0), fx=scale, fy=scale)
 
         # Apply filters
-        grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blured = cv2.medianBlur(grey, 15)
 
         # Compose 2x2 grid with all previews
-        grid = np.zeros([w*2, h*2, 3], np.uint8)
-        grid[0:w, 0:h] = frame
+        grid = np.zeros([h*2, w*2, 3], np.uint8)
+        grid[0:h, 0:w] = image
 
         # We need to convert each of them to RGB from grescaled 8 bit format
         """rispetto al codice trovato online ho dovuto invertire width con height"""
-        grid[w:2 * w, 0:h] = np.dstack([cv2.Canny(grey, t / 2, t)] * 3)
-        grid[0:w, h:2 * h] = np.dstack([blured] * 3)
-        grid[w:2 * w, h:2 * h] = np.dstack([cv2.Canny(blured, t / 2, t)] * 3)
+        grid[h:2 * h, 0:w] = np.dstack([cv2.Canny(grey, t / 2, t)] * 3)
+        grid[0:h, w:2 * w] = np.dstack([blured] * 3)
+        grid[h:2 * h, w:2 * w] = np.dstack([cv2.Canny(blured, t / 2, t)] * 3)
         """ sarebbe meglio cambiare la dimensione, ma come faccio?"""
         cv2.imshow('Image previews', grid)
 
-        if img is None:
+        if image is None:
             #convertire il fram in RGB xké è in BGR
-            img = plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            image = plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
             plt.axis("off")
             plt.title('Camera stream')
         else:
-            img.set_data(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))#rimpiazza il contenuto di imgshow senza rimpiazzare gli assi il titolo ...
+            image.set_data(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))#rimpiazza il contenuto di imgshow senza rimpiazzare gli assi il titolo ...
             fig.canvas.draw() # disegna il nuovo dato
             plt.pause(1/30)
 
---
+++ pyimagesearch/shapedetector.py
@@ -0,0 +1,37 @@
+from typing import Any, Union
+
+import cv2
+import math
+
+class ShapeDetector:
+    def __init__(self):
+        pass
+
+    def detect(self, c):
+        # c stay for contour
+        # initialize the shape name and approximate the contour
+        shape = "unidentified"
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+        #skill small or non convex object
+        if math.fabs(cv2.contourArea(c) < 100 or not (cv2.isContourConvex(approx))):
+            return shape
+
+        if len(approx) == 4:
+            # compute the bounding box of the contour and use the
+            # bounding box to compute the aspect ratio
+            (x, y, w, h) = cv2.boundingRect(approx)
+            ar = w / float(h)
+            area = cv2.contourArea(c);
+            # x , y top left
+            e = 0.10;
+            b1 = b2 = 0;
+            if (abs(x - w) < 0.10):
+                b1 = x;
+                b2 = w;
+
+            # a square will have an aspect ratio that is approximately
+            # equal to one, otherwise, the shape is a rectangle
+            shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
+        return shape
