9eccfa5ca80650f602b9878467a6ce8d061dc945
salvolannister <salvatore.d.gurzi@gmail.com>
2019-08-07 10:40:43 -0300

    inizializzata la variabile
    rotY = 0

--- threejs_prova.js
+++ threejs_prova.js
@@ -1,14 +1,14 @@
+var model, modelRotY=0;
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer({alpha : true});
+var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer({antialias: true, alpha : true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 //renderer.setPixelRatio( window.devicePixelRatio );
 renderer.gammaOutput = true;
 renderer.gammaFactor = 2.2;
 document.body.appendChild( renderer.domElement );
 
-camera.position.z = 1;
+camera.position.z= 1;
 
 
 
@@ -16,8 +16,9 @@ var loader = new THREE.GLTFLoader();
 loader.load(
    "./vaso.glb",
    function ( gltf ) {
-    scene.add(gltf.scene);
-
+    model = gltf.scene
+    scene.add(model);
+    //model.rotation.y = 4.5
     gltf.animations; // Array<THREE.AnimationClip>
 		gltf.scene; // THREE.Scene
 		gltf.scenes; // Array<THREE.Scene>
@@ -27,8 +28,38 @@ loader.load(
    },
 );
 
+function initLight() {
+    light = new THREE.SpotLight(0xffffff);
+    // Position the light slightly to a side to make shadows look better.
+    light.position.set(400, 100, 1000);
+    light.castShadow = true;
+    scene.add(light);
+}
+initLight();
+
+function onWindowResize() {
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+				renderer.setSize( window.innerWidth, window.innerHeight );
+			}
+
+function update() {
+        /* bisogna aspettare che
+        il modello sia caricato */
+        if (model) {
+
+          modelRotY += 0.01;
+          model.rotation.y = modelRotY;
+        }
+
+      }
+
+
+
 function animate() {
+  update();
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
+
 }
 animate();
