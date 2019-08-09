e154b2372234e45cb616dd54e09bc865af80b9dd
salvolannister <salvatore.d.gurzi@gmail.com>
2019-08-09 11:07:11 -0300

    working !

--- detect_shapes.py
+++ detect_shapes.py
@@ -5,7 +5,8 @@ import threading
 import numpy as np
 
 from pyimagesearch.shapedetector import ShapeDetector
-from SimpleWebSocketServer import SimpleExampleServer, WebSocket
+from SimpleWebSocketServer import SimpleExampleServer, WebSocket, SimpleWebSocketServer
+
 # load the image and resize it to a smaller factor so that
 # the shapes can be approximated better
 
@@ -28,6 +29,9 @@ def run_server():
 
 t = threading.Thread(target=run_server)
 t.start()
+kernel1 = np.ones((8, 8), np.uint8)
+kernel2 = np.ones((2, 2), np.uint8)
+
 
 while True:
     ret, image = capture.read()
@@ -35,24 +39,43 @@ while True:
 
 
     # Apply filters
+    hls_img = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
     grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blured = cv2.medianBlur(grey, 15)
+    blured = cv2.medianBlur(hls_img, 15)
+    lower_red = np.array([170, 70, 50])
+    upper_red = np.array( [180, 255, 255])
+    mask_0 = cv2.inRange(hls_img, lower_red, upper_red)
 
+    lower_red1 = np.array([0, 50, 50])
+    upper_red1 = np.array([10, 255, 255])
+    mask_1 = cv2.inRange(hls_img, lower_red1, upper_red1)
+    mask_red = mask_0 + mask_1
+    res = cv2.bitwise_and(image, image, mask=mask_red)
 
     cv2.imshow('Image previews', image)
-
+    cv2.imshow('bit_wise', res)
     resized = imutils.resize(image, width=300)
     ratio = image.shape[0] / float(resized.shape[0])
 
     # convert the resized image to grayscale, blur it slightly,
     # and threshold it
-    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+    r_hls_img = cv2.cvtColor(resized, cv2.COLOR_BGR2HLS)
+    #blurred = cv2.GaussianBlur(resized, (3, 3), 0)
+    mask_0 = cv2.inRange(r_hls_img, lower_red, upper_red)
+    mask_1 = cv2.inRange(r_hls_img, lower_red1, upper_red1)
+    mask_red = mask_0 + mask_1
+
+    erosion = cv2.erode(mask_red, kernel2, iterations=1)
+    dilatation =  cv2.dilate(erosion, kernel1,iterations = 1)
+   # res = cv2.bitwise_and(dilatation, resized, mask=mask_red)
     #thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
-    canny = cv2.Canny(blurred, 80, 180, 3)
+    canny = cv2.Canny(dilatation , 80, 180, 3)
 
+    cv2.imshow('red mask', mask_red)
+    cv2.imshow('erosion', erosion)
+    cv2.imshow('dilatation', dilatation)
     cv2.imshow("canny", canny)
-
+    cv2.imshow("bit wise", res)
     # find contours in the thresholded image and initialize the
     # shape detector
     cnts = cv2.findContours(canny, cv2.RETR_EXTERNAL,
@@ -72,7 +95,7 @@ while True:
         cY = int((M["m01"] / M["m00"]) * ratio)
 
         shape = sd.detect(c)
-        print(shape)
+        #print(shape)
         if shape == "rectangle":
             # multiply the contour (x, y)-coordinates by the resize ratio,
             # then draw the contours and the name of the shape on the image
